68fc6793ee521c8e82983e654772d426711840ba
tharun232 <tharun232@users.noreply.github.com>
2024-04-21 20:04:12 +0530

    Update s3_bucket.tf

--- s3_bucket.tf
+++ s3_bucket.tf
@@ -8,3 +8,10 @@ resource "aws_s3_bucket" "my_bucket" {
     Name = "My Terraform Managed Bucket"
   }
 }
+
+resource "aws_s3_bucket_acl" "my_bucket_acl" {
+  bucket = aws_s3_bucket.my_bucket.bucket
+  
+  acl    = "private"  # Set the ACL to "private" or any other desired ACL
+}
+
