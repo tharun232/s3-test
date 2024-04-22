76f00403c4cc1c396759d55ebcb5157109103a9a
tharun232 <tharun232@users.noreply.github.com>
2024-04-22 18:55:49 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-s3-2024"
+    bucket = "terraform-s3-2026"
     region = "ap-south-1"
     key = "terraform/terraform.tfstate"
     dynamodb_table = "terraform_lock_2024"
