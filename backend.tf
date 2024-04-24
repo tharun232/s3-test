21f18097f9c49e472f71e0e761855dad7a1db7d3
tharun232 <tharun232@users.noreply.github.com>
2024-04-24 18:04:44 +0000

    Delete backend.tf

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "terraform-s3-2026"
-    region = "ap-south-1"
-    key = "terraform/terraform.tfstate"
-    dynamodb_table = "terraform_lock_2024"
-  }
-}
