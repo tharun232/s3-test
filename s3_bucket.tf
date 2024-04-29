c84986c8e0ddabf00c8122084e8816a72255e8d0
tharun232 <tharun232@users.noreply.github.com>
2024-04-29 13:18:57 +0530

    Update s3_bucket.tf

--- s3_bucket.tf
+++ s3_bucket.tf
@@ -1,10 +1,3 @@
 resource "aws_s3_bucket" "bktCreate" {
   bucket = var.bkt
-  tags = {
-    Environment        = "${local.environment}"
-    AppName            = "${local.appname}"
-    Backup             = "${local.backup}"
-    BusinessUnit       = "${local.businessunit}"
-    DataClassification = "${local.dataclassification}"
-  }
 }
