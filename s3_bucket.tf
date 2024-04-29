a12dc37764b3de21de9c4bc1b09746281d33f513
tharun232 <tharun232@users.noreply.github.com>
2024-04-29 13:33:16 +0530

    Update s3_bucket.tf

--- s3_bucket.tf
+++ s3_bucket.tf
@@ -1,3 +1,28 @@
-resource "aws_s3_bucket" "bktCreate" {
-  bucket = var.bkt
+resource "aws_s3_bucket" "example" {
+  bucket = var.bucket_name
+
+  force_destroy = true
+
+  tags = {
+    Name = "My Terraform Managed Bucket"
+    Name = "Test"
+    Name = "Test1"
+    Env  = "Cloud"
+
+  }
 }
+
+resource "aws_s3_bucket_ownership_controls" "example" {
+  bucket = aws_s3_bucket.example.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "example" {
+  depends_on = [aws_s3_bucket_ownership_controls.example]
+
+  bucket = aws_s3_bucket.example.id
+  acl    = "private"
+}
+
