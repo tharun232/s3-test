361d47da5bf5bd0ffaa491e2d5ca214415fe5fe9
tharun232 <tharun232@users.noreply.github.com>
2024-05-06 14:53:03 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,10 +1,11 @@
 variable "bucket_name" {
   description = "The name of the S3 bucket"
   type        = string
+  default     = ""
 }
 
 variable "aws_region" {
   description = "The AWS region where the bucket is created"
   type        = string
-  default     = "us-west-2"
+  default     = ""
 }
